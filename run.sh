b76729259d0a1b5b26d4a9bf4ee00f72b59845c6
purelistica <anna.smirnova.hse@gmai.com>
2019-12-15 14:30:12 +0300

    Run scripts

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-python3 -m venv ~/.venv/HseNlpWeather --upgrade
-source ~/.venv/HseNlpWeather/bin/activate
-pip3 install -r requirements.txt
+python3 -m venv ~/.venv/HseNlpWeather
+source ~/.venv/HseNlpWeather/bin/activate && pip3 install -r requirements.txt
